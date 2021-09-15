f8fed6a85d37851c1f9ba22340ab711d1e145748
Brendan <bjregnery@ucdavis.edu>
2021-09-15 14:01:52 +0200

    Starting to make CRAB files

---
+++ CrabRunFiles/submit_step1_SIM_crab.py
@@ -0,0 +1,27 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+config.section_('General')
+
+config.General.requestName = 'NLO_step1_VLQ_SIM'
+config.General.workArea = 'crab_projects'
+config.General.transferOutputs = True
+config.General.transferLogs = False
+
+config.section_('JobType')
+config.JobType.pluginName = 'PrivateMC'
+config.JobType.psetName = 'submit_step1_SIM_crab.py'
+
+config.section_('Data')
+#config.Data.userInputFiles = ['run_generic_tarball_cvmfs.sh']
+config.Data.outputPrimaryDataset = 'MinBias'
+config.Data.splitting = 'EventBased'
+config.Data.unitsPerJob = 10
+NJOBS = 100  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
+config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
+config.Data.outLFNDirBase = '/store/user/bregnery/' 
+config.Data.publication = True
+config.Data.outputDatasetTag = 'NLO_step1_VLQ_SIM'
+
+config.section_('Site')
+config.Site.storageSite = 'T3_US_FNALLPC'
+
